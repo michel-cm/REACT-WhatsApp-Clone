05577ee54425c9069cad36b74f7d6686a4c94d75
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-06-29 13:51:10 -0300

    Add files via upload
    Interface Chat completed

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -1,45 +1,64 @@
-import React from 'react';
-import './ChatWindow.css'
+import React from "react";
+import "./ChatWindow.css";
 
 import SearchIcon from "@mui/icons-material/Search";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import CloseIcon from "@mui/icons-material/Close";
+import SendIcon from "@mui/icons-material/Send";
+import MicIcon from "@mui/icons-material/Mic";
 
 function ChatWindow() {
-    return(
-        <div className='chatWindow'>
-
-            <div className='chatWindow--header'>
-
-                <div className='chatWindow--headerinfor'>
-                    <img className='chatWindow--avatar' src='https://www.w3schools.com/w3images/avatar5.png' alt='' />
-                    <div className='chatWindow--name'>Michel Corrêa </div>
-                </div>
-
-                <div className='chatWindow--headerbuttons'>
-                    <div className='chatWindow--btn'>
-                        <SearchIcon  style={{color: '#919191'}}/>
-                    </div>
-                    <div className='chatWindow--btn'>
-                        <AttachFileIcon  style={{color: '#919191'}}/>
-                    </div>
-                    <div className='chatWindow--btn'>
-                        <MoreVertIcon  style={{color: '#919191'}}/>
-                    </div>
-                </div>
-
-            </div>
-
-            <div className='chatWindow--body'>
-
-            </div>
-
-            <div className='chatWindow--footer'>
-
-            </div>
-
+  return (
+    <div className="chatWindow">
+      <div className="chatWindow--header">
+        <div className="chatWindow--headerinfor">
+          <img
+            className="chatWindow--avatar"
+            src="https://www.w3schools.com/w3images/avatar5.png"
+            alt=""
+          />
+          <div className="chatWindow--name">Michel Corrêa </div>
         </div>
-    );
+
+        <div className="chatWindow--headerbuttons">
+          <div className="chatWindow--btn">
+            <SearchIcon style={{ color: "#919191" }} />
+          </div>
+          <div className="chatWindow--btn">
+            <AttachFileIcon style={{ color: "#919191" }} />
+          </div>
+          <div className="chatWindow--btn">
+            <MoreVertIcon style={{ color: "#919191" }} />
+          </div>
+        </div>
+      </div>
+
+      <div className="chatWindow--body"></div>
+
+      <div className="chatWindow--footer">
+        <div className="chatWindow--pre">
+          <div className="chatWindow--btn">
+            <InsertEmoticonIcon style={{ color: "#919191" }} />
+          </div>
+        </div>
+
+        <div className="chatWindow--inputarea">
+            <input 
+            className="chatWindow--input" 
+            type="text" 
+            placeholder="Digite uma mensagem" 
+            />
+        </div>
+        <div className="chatWindow--pos">
+          <div className="chatWindow--btn">
+            <SendIcon style={{ color: "#919191" }} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatWindow;
