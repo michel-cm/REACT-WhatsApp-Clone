af92764d202076b4a57ef105b74509c7c902f612
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-07-18 20:37:03 -0300

    Firebase Config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLE3K3RWMpcF34mlMFdntImrtHINFfzIE",
+    authDomain: "whatsappclone-1979a.firebaseapp.com",
+    projectId: "whatsappclone-1979a",
+    storageBucket: "whatsappclone-1979a.appspot.com",
+    messagingSenderId: "993999566718",
+    appId: "1:993999566718:web:08944ea1f88e188ae0e52f"
+  };
+
+  export default firebaseConfig;
